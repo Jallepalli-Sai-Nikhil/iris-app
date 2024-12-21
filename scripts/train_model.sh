0f380aa0aa709bd910002be67a10806cf327e722
Sai-Nikhil29 <sainikhil.jallepalli@gmail.com>
2024-12-22 00:07:54 +0530

    v.0.0.4

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -14,6 +14,6 @@ source venv/Scripts/activate || { echo "Failed to activate virtual environment";
 
 # Run the training script
 echo "Training the model..."
-dvc repro || { echo "Model training failed"; exit 1; }
+python train.py || { echo "Model training failed"; exit 1; }
 
 echo "Model training completed successfully!"
